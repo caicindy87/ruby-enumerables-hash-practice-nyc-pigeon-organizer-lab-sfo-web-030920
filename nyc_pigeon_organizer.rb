4eca06f954fad76d1881c56ef9fa62ef4f163bb1
Cindy Cai <caicindy87@gmail.com>
2020-03-06 19:10:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,6 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |attributes, info|
    info.each do |color_gender_locations, name|
-  
      name.each do |pigeon_name|
       if !pigeon_list[pigeon_name]
         pigeon_list[pigeon_name] = {}
@@ -11,7 +10,6 @@ def nyc_pigeon_organizer(data)
       if !pigeon_list[pigeon_name][attributes]
         pigeon_list[pigeon_name][attributes] = []
       end
-      pigeon_list[pigeon_name][attributes].push(color_gender_locations.to_s)
       
     end
    end
